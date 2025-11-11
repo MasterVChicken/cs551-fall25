4373d3bc60d16ad74549d460e37449b03540a62f
MasterVChicken <yanliang.l@outlook.com>
2025-11-10 16:26:29 -0800

    Add persistence support for page. May need header support while write files

--- lstore/page.py
+++ lstore/page.py
@@ -4,44 +4,40 @@ class Page:
     # All columns are 64-bit integers
     # 1 Page only store 1 column
     # Simulate as pages in hardware view
-    # Using little-endian and unsigned for byte stream by default
+    # Using little-endian and signed for byte stream by default
     def __init__(self):
-        self.num_records = 0
+        self.num_items = 0
         self.data = bytearray(4096)
-        self.max_records = 4096 // 8
+        # I want to add header during storage
+        # Considering set the first 8 bytes as header within the file
+        self.max_items = 4096 // 8
 
     def has_capacity(self):
-        return self.num_records < self.max_records
+        return self.num_items < self.max_items
     
     def get_capacity(self):
-        return self.max_records - self.num_records
+        return self.max_items - self.num_items
         
     def is_full(self):
-        return self.num_records >= self.max_records
+        return self.num_items >= self.max_items
 
     def write(self, value):
         # Make sure we have space
         if not self.has_capacity():
             return False
 
-        offset = self.num_records * 8
+        offset = self.num_recnum_itemsords * 8
 
-        # value_bytes = value.to_bytes(8, byteorder="little", signed=False)
         value_bytes = value.to_bytes(8, byteorder="little", signed=True)
         self.data[offset : offset + 8] = value_bytes
 
-        self.num_records += 1
+        self.num_items += 1
         return True
     
     def read(self, index):
-        if index < 0 or index >= self.num_records:
+        if index < 0 or index >= self.num_items:
             return None
         offset = index * 8
-        # return int.from_bytes(
-        #     self.data[offset:offset + 8],
-        #     byteorder='little',
-        #     signed=False
-        # )
         return int.from_bytes(
             self.data[offset:offset + 8],
             byteorder='little',
@@ -49,17 +45,24 @@ class Page:
         )
         
     def update(self, index, value):
-        if index < 0 or index >= self.num_records:
+        if index < 0 or index >= self.num_items:
             return False
         offset = index * 8
-        # value_bytes = value.to_bytes(8, byteorder='little', signed=False)
         value_bytes = value.to_bytes(8, byteorder='little', signed=True)
         self.data[offset:offset + 8] = value_bytes
         return True
+    
+    # Persistence helper functions for read and write raw bytearray
+    def get_data(self):
+        return self.data
+    
+    def set_data(self, data, num_items):
+        self.data = data
+        self.num_items = num_items
         
 
     
-# BasePage and TailPage are identical in physical view but is different in logic view
+# BasePage and TailPage are identical in physical view but different in logic view
 # Notice: We also import metadata columns from L-store design
 class BasePage():
     # Only write when insert new records
@@ -71,7 +74,6 @@ class BasePage():
         self.RID_COLUMN = 1
         self.TIMESTAMP_COLUMN = 2
         self.SCHEMA_ENCODING_COLUMN = 3
-        # self.USER_COLUMN_START = 4
 
         self.BASE_RID_COLUMN = 4
         self.USER_COLUMN_START = 5
@@ -99,6 +101,23 @@ class BasePage():
         self.num_records += 1
         return True
     
+    # Persistence helper functions for BasePage
+    def get_page_data(self, column_index):
+        if column_index >= len(self.physical_pages):
+            return None
+        return self.physical_pages[column_index].get_data()
+    
+    def set_page_data(self, column_index, data, num_records):
+        if column_index >= len(self.physical_pages):
+            return False
+        
+        self.physical_pages[column_index].set_data(data, num_records)
+        # Align num of records with the first column
+        if column_index == 0:
+            self.num_records = num_records
+        return True
+    
+    
     # # Maybe we should define read()
     # def read_record():
     
@@ -113,7 +132,6 @@ class TailPage():
         self.RID_COLUMN = 1
         self.TIMESTAMP_COLUMN = 2
         self.SCHEMA_ENCODING_COLUMN = 3
-        # self.USER_COLUMN_START = 4
 
         self.BASE_RID_COLUMN = 4
         self.USER_COLUMN_START = 5
@@ -140,6 +158,22 @@ class TailPage():
             self.physical_pages[self.USER_COLUMN_START + i].write(actual_value)
         
         self.num_records += 1
+        return True
+    
+    # Persistence helper functions for TailPage
+    def get_page_data(self, column_index):
+        if column_index >= len(self.physical_pages):
+            return None
+        return self.physical_pages[column_index].get_data()
+    
+    def set_page_data(self, column_index, data, num_records):
+        if column_index >= len(self.physical_pages):
+            return False
+        
+        self.physical_pages[column_index].set_data(data, num_records)
+        # Align num of records with the first column
+        if column_index == 0:
+            self.num_records = num_records
         return True
     
     # # Maybe we should define read()
